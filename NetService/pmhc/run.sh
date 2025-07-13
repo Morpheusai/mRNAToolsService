96de4fa1aaaf16ada2165e66362a9c58b685f190
scener-y <1838648927@qq.com>
2025-07-13 12:50:46 +0800

    添加启动命令

--- NetService/pmhc/run.sh
+++ NetService/pmhc/run.sh
@@ -1,2 +1,6 @@
-#nohup uvicorn app:app  --host 0.0.0.0 --port 60001 1>log_net 2>err_net &
-uvicorn app:app  --host 0.0.0.0 --port 60001
+nohup uvicorn app:app \
+  --timeout-keep-alive 21600 \
+  --timeout-graceful-shutdown 1200 \
+  --host 0.0.0.0 \
+  --port 60823 \
+  >>log 2>>err &
